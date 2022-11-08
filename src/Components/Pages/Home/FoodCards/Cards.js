ed4fd0e42f075ca0b1d98cd91af28c942075769c
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2022-11-08 11:00:53 +0600

    service cards added

--- src/Components/Pages/Home/FoodCards/Cards.js
+++ src/Components/Pages/Home/FoodCards/Cards.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-const Cards = () => {
+const Cards = ({ food }) => {
+    console.log(food);
+    const { img, title, description, price, review } = food;
     return (
-        <div className="card w-96 bg-base-100 shadow-xl image-full">
-            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+        <div className="card lg:w-2/3 mx-auto bg-base-100 shadow-xl image-full">
+            <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <h2 className="card-title">{title}</h2>
+                <p>
+                    {
+                        description.length > 100 ? description.slice(0, 100) : description
+                    }
+                </p>
+                <div className="card-actions justify-between items-center">
+                    <h4>Price: ${price}</h4>
+                    <button className="btn btn-primary">More Details</button>
                 </div>
             </div>
         </div>
--- src/Components/Pages/Home/FoodCards/FoodCards.js
+++ src/Components/Pages/Home/FoodCards/FoodCards.js
@@ -1,14 +1,18 @@
 import { useLoaderData } from 'react-router-dom';
 import Cards from './Cards';
+import './FoodCards.css';
 
 const FoodCards = () => {
     const foods = useLoaderData([]);
     return (
-        <div>
-            <h1>{foods.length}</h1>
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-10 mx-auto justify-center items-center text-center'>
+        <div className='foodBg py-20'>
+            <h1 className=' mb-10 text-5xl text-white font-bold'>Welcome to <span className='text-primary'>FoodMart</span>!!!</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-10'>
                 {
-                    foods.map(food => <Cards />)
+                    foods.map(food => <Cards
+                        key={food.service_id}
+                        food={food}
+                    />)
                 }
             </div>
         </div>
